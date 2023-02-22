028ec7e6bb00571578e400ad17847f571dc9b4a0
kazimbukhari7 <125092896+kazimbukhari7@users.noreply.github.com>
2023-02-22 14:22:12 -0800

    Update erw_pers.objectname.sql
    - update to this

--- erw_pers.objectname.sql
+++ erw_pers.objectname.sql
@@ -1,78 +1,108 @@
-do $$
+/*
+ Parameters:
+ Name               |   Type        |   Description
+ p_debug            |   boolean     |   Default (false) parameter for debugging function no functional paramater
+ p_start_read_date  |   DATE        |   Default NULL. if passed then function will process data between provided read date range.
+ p_end_read_date    |   DATE        |   Default NULL. if passed then function will process data between provided read date range.
 
 
-declare
+ Return: BOOLEAN
+ Purpose: This function used to update (in insert and delete mode) the MDMS reads timestamps which were loaded incorrectly after ERW go-live due to IDR limitation of adding timezone offet.
 
+       - Since ERW go-live, the reads in ERW are added by additional 7 or 8 hours.
+       - So, we need to perform a data fix for historical data which are inserted after ERW-golive via regular IDR feed.
+       - Exclude the data which are inserted from Hadoop as part of Initial load. By  etl_process_key we can differentiate historical load and regular IDR process load.
+
+ Dependent Objects:
+     Type    |  Name
+     Table   |  erw_pers.tb_interval_read
+     
+
+
+
+ ChangeLog:
+     Date       |     Author        |    Ticket         | Modification
+     2022-02-21 |     Thiru K       |    ERW-39        | Initial development
+ */
+CREATE OR REPLACE FUNCTION erw_pers.fn_update_mdms_interval_read(p_debug boolean DEFAULT false,  p_start_read_date date DEFAULT NULL,  p_end_read_date date DEFAULT NULL)
+RETURNS BOOLEAN AS
+$BODY$
+
+DECLARE
 v_sql text ; 
-v_etl_process_key bigint ;
-v_min_etl_insert_ts timestamp;  
---v_max_etl_insert_ts timestamp := select COALESCE((select  deploy_date from public."version"   where file='0056_up.sql' and "version"='1.0.103'), '9999-12-31 00:00:00') ;
-v_max_etl_insert_ts timestamp := '9999-12-31' ;
-v_start_ts timestamp ;
-v_end_ts timestamp  :=  now();
+v_msg text;
+v_row_cnt bigint := 0;
+v_sstart_ts timestamp with time zone;
+v_fstart_ts timestamp with time zone := date_trunc('second', clock_timestamp());
 
-begin
-	
-		
-	SELECT  etl_process_key into  v_etl_process_key FROM   erw_util.tb_etl_process      WHERE    etl_process_name='fn_mdms_interval_read';
-	SELECT COALESCE(min(parameter_value),'1900-01-01 00:00:00') into 	v_min_etl_insert_ts FROM  erw_util.tb_etl_parameter WHERE parameter_name='mdms_interval_read_process_ts';
-	
-	
-	v_sql :=  $_sql_$ 
-				select min(read_end_ts)
-				 from erw_pers.tb_interval_read tir 
-				 where etl_insert_ts >=  '$_sql_$ || v_min_etl_insert_ts  || $_sql_$'
-				 and read_end_ts between '2017-01-01 00:00:00'
-				 and '2021-10-01 00:00:00'
-				 $_sql_$;
-				
-	raise notice '%', v_sql;
-	
-	execute v_sql into v_start_ts;
-	
+--Get the ETL process key for MDMS interval reads
+v_etl_process_key integer := (SELECT erw_util.fn_get_process_key('erw_pers','fn_mdms_interval_read',p_debug));
 
+--Get the minimum ETL process date for MDMS interval reads since ERW go-live
+v_min_etl_insert_ts timestamp := (SELECT COALESCE(min(parameter_value),'1900-01-01 00:00:00') FROM  erw_util.tb_etl_parameter WHERE parameter_name='mdms_interval_read_process_ts');
+
+--Get the maximum ETL process key for ERW-39 fix deployed (this is applicable in Reads_stg) - not used for PROD and SIT
+v_fix_deployment_date timestamp := (SELECT COALESCE((SELECT  date_trunc('second', deploy_date) FROM public."version"   WHERE file='0056_up.sql' AND "version"='1.0.103'), now())) ;
+v_min_available_read_date Date;
+v_read_start_date Date ;
+v_read_end_date Date;
+
+BEGIN
+
+    v_sstart_ts := date_trunc('second', clock_timestamp());
+    v_msg := FORMAT('%s.....Starting erw_pers.fn_update_mdms_interval_read Execution....', v_fstart_ts);
+    PERFORM erw_util.logger('INFO', v_msg, p_debug);
+
+    v_msg := FORMAT('%s.....ETL Process key for MDMS interval reads %s', v_fstart_ts, v_etl_process_key);
+    PERFORM erw_util.logger('INFO', v_msg, p_debug);
+
+    v_msg := FORMAT('%s.....Minimum available ETL timestamp for MDMS interval reads %s', v_fstart_ts, v_min_etl_insert_ts);
+    PERFORM erw_util.logger('INFO', v_msg, p_debug);
+
+    v_msg := FORMAT('%s.....ERW 39 Fix Function change deployment date for MDMS interval reads %s', v_fstart_ts, v_fix_deployment_date);
+    PERFORM erw_util.logger('INFO', v_msg, p_debug);
 	
-	v_sql := $_sql_$
-		select 
-		spid
-		--   ,read_end_ts --original
-		, CASE 
-		WHEN EXTRACT(TIMEZONE  FROM read_end_ts ) = '-28800' THEN
-		((read_end_ts- INTERVAL '28800' second))  :: timestamp with time zone 
-		WHEN EXTRACT(TIMEZONE  FROM read_end_ts ) = '-25200' THEN
-		((read_end_ts - INTERVAL '25200' second )):: timestamp with time zone 
-		end 
-		as updated_read_end_ts
-		, source_commit_ts
-		, etl_insert_ts
-		, read_val
-		, read_version_num
-		, interval_length
-		, read_status_id
-		--, etl_process_key 
-		, 999
-		, channel_type_cd
-		, op_code
-		, uom
-			, channel_type_desc
-			from erw_pers.tb_interval_read
-			where
-			--to avoid Hadoop initial load data
-			etl_process_key = $_sql_$ ||  v_etl_process_key ||  $_sql_$
-			AND 
-			--to consider all the data which are loaded after ERW go-live and before ERW-39 function deployment
-			etl_insert_ts 
-			between '$_sql_$ || v_min_etl_insert_ts || $_sql_$'
-			and '$_sql_$ ||  v_max_etl_insert_ts || $_sql_$' 
-			and read_end_ts between '$_sql_$ || v_start_ts || $_sql_$'
-			and '$_sql_$ || v_end_ts || $_sql_$'
-    $_sql_$ ;
-   
-  	raise notice  '%', v_sql;
-   
-end $$;
+	IF p_start_read_date IS NOT NULL AND p_end_read_date IS NOT NULL THEN
+		v_read_start_date := p_start_read_date ;
+		v_read_end_date := p_end_read_date ;
+	ELSE
+		v_read_start_date := '2011-01-01' ;
+		v_read_end_date := current_date ;
+	END IF ;
+	/*
+	Find the minimum read date of available reads in the erw_pers.tb_interval_read table
+	*/
+	v_sstart_ts := date_trunc('second', clock_timestamp());
+	v_sql :=  $_$ 
+				SELECT MIN(read_end_ts)
+				 FROM erw_pers.tb_interval_read  
+				 WHERE etl_insert_ts >= '$_$ || v_min_etl_insert_ts  || $_$' 
+				 AND read_end_ts::Date BETWEEN '$_$ || v_read_start_date  || $_$' AND '$_$ || v_read_end_date  || $_$'
+				 AND etl_process_key = $_$ ||  v_etl_process_key ||  $_$
+			  $_$;
+	PERFORM erw_util.logger('INFO', v_sql, p_debug);
+	BEGIN
+       IF v_sql IS NOT NULL THEN
+                EXECUTE v_sql INTO v_min_available_read_date;
+            ELSE
+                v_msg := FORMAT('| Selecting minimum reads date from interval read table query is NULL');
+                PERFORM erw_util.logger('ERROR', v_msg, p_debug);
+                RETURN(FALSE);
+		END IF;
+		v_msg := FORMAT('| OPERATION_STATS | %s | %s | %s | %s | %s | %s | %s |', 'fn_update_mdms_interval_read', v_sstart_ts, date_trunc('second',clock_timestamp()), date_trunc('second',age(clock_timestamp(), v_sstart_ts)), v_min_available_read_date, 'SELECT', 'Minimum available reads timestamp');
+        PERFORM erw_util.logger('INFO', v_msg, p_debug);
+	   
+	    EXCEPTION
+            WHEN OTHERS THEN
+                RAISE EXCEPTION '%; SQLSTATE: %', SQLERRM, SQLSTATE;
+                RETURN False;
+      
+    END;
+	
+	--
+	
+	RETURN True;
+END
 
-
-
-
- 
+$BODY$
+LANGUAGE plpgsql;
